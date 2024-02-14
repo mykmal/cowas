395fcef24eed3dc715012a473257c520340947aa
Mykhaylo M. Malakhov <malak039@umn.edu>
2024-02-14 12:07:16 -0600

    Perform pQTL mapping for all proteins simultaneously

--- map_pqtls.sh
+++ map_pqtls.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=124gb
+#SBATCH --cpus-per-task=128
+#SBATCH --mem=420gb
 #SBATCH --time=96:00:00
 #SBATCH --partition=agsmall,aglarge,ag2tb
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=malak039@umn.edu
 #SBATCH -o logs/%j.out
 
-# Specify the protein for which to perform association testing
-# This variable must match the column name for the protein's expression levels
-PROTEIN=protein_1
-
 module load R/4.3.0-openblas
 
-if ( [ ! -d pqtls ] ); then
 mkdir pqtls
-fi
 
 # Perform association testing
 plink2 --pfile data_cleaned/ukb_filtered \
-       --no-psam-pheno \
-       --threads 30 \
-	   --memory 120000 \
+	   --no-psam-pheno \
+	   --threads 128 \
+	   --memory 418000 \
 	   --pheno data_cleaned/proteins.tsv \
-	   --pheno-name ${PROTEIN} \
 	   --pheno-quantile-normalize \
 	   --covar data_cleaned/covariates.tsv \
 	   --covar-variance-standardize \
-       --glm omit-ref hide-covar cols=p \
+	   --glm omit-ref hide-covar cols=p \
 	   --vif 100000 \
 	   --pfilter 0.01 \
-	   --out TEMP_${PROTEIN}
+	   --out TEMP
 
-# Rename and move the results file
-mv TEMP_${PROTEIN}.${PROTEIN}.glm.linear pqtls/${PROTEIN}_sumstats.tsv
-rm TEMP_${PROTEIN}*
+# Rename and move the results files
+for NUMBER in {1..2923}; do
+mv TEMP.protein_${NUMBER}.glm.linear pqtls/protein_${NUMBER}_sumstats.tsv
+done
+
+rm TEMP*
 
