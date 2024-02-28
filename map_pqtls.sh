b86e6b37aa9142d356f303d4bea559141687cf57
Mykhaylo M. Malakhov <malak039@umn.edu>
2024-02-28 16:05:46 -0600

    Exclude collinear covariates

--- map_pqtls.sh
+++ map_pqtls.sh
@@ -23,19 +23,19 @@ plink2 --pfile data_cleaned/ukb_filtered \
 	   --pheno-quantile-normalize \
 	   --covar data_cleaned/covariates.tsv \
 	   --covar-variance-standardize \
-	   --glm omit-ref hide-covar skip-invalid-pheno cols=chrom,pos,beta,p \
+	   --glm omit-ref hide-covar cols=chrom,pos,beta,p \
 	   --vif 100000 \
 	   --out TEMP
 
 # Rename and move the results files
 for NUMBER in {1..2923}; do
-	if ( [ -f TEMP.protein_${NUMBER}.glm.linear ] ); then
-		mv TEMP.protein_${NUMBER}.glm.linear pqtls/protein_${NUMBER}_sumstats.tsv
+    if ( [ -f TEMP.protein_${NUMBER}.glm.linear ] ); then
+	    mv TEMP.protein_${NUMBER}.glm.linear pqtls/protein_${NUMBER}_sumstats.tsv
 	fi
 done
 
-# protein_1173 must be treated separately because otherwise it causes PLINK to crash
-# due to some covariates being constant after removing NA rows
+# protein_1173 only has 106 non-missing samples, and within that subset some covariates are constant or collinear.
+# Those covariates must be removed before performing a GWAS for this protein, or else PLINK will crash
 plink2 --pfile data_cleaned/ukb_filtered \
        --no-psam-pheno \
 	   --threads 128 \
@@ -44,7 +44,7 @@ plink2 --pfile data_cleaned/ukb_filtered \
 	   --pheno-name protein_1173 \
 	   --pheno-quantile-normalize \
 	   --covar data_cleaned/covariates.tsv \
-	   --not-covar ASSESSMENT_CENTER_11001 ASSESSMENT_CENTER_11002 ASSESSMENT_CENTER_11005 ASSESSMENT_CENTER_11022 ASSESSMENT_CENTER_11023 \
+	   --not-covar ASSESSMENT_CENTER_11001 ASSESSMENT_CENTER_11002 ASSESSMENT_CENTER_11005 ASSESSMENT_CENTER_11018 ASSESSMENT_CENTER_11020 ASSESSMENT_CENTER_11022 ASSESSMENT_CENTER_11023 \
 	   --covar-variance-standardize \
 	   --glm omit-ref hide-covar cols=chrom,pos,beta,p \
 	   --vif 100000 \
