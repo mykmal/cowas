71e728f7dab507b731cc05fe7d56b4eb8cf5829a
Mykhaylo M. Malakhov <malak039@umn.edu>
2025-05-30 10:48:42 -0500

    Remove GLIPR1 code

--- utils/map_pqtls.sh
+++ utils/map_pqtls.sh
@@ -17,7 +17,6 @@ plink2 --pfile data_cleaned/genotypes \
        --threads 128 \
        --memory 420000 \
        --pheno data_cleaned/proteins.tsv \
-       --not-pheno GLIPR1 \
        --pheno-quantile-normalize \
        --covar data_cleaned/covariates.tsv \
        --covar-variance-standardize \
@@ -30,24 +29,6 @@ for FILENAME in *.glm.linear; do
     NEWFILE=$(echo ${FILENAME} | sed 's/TEMP.//' | sed 's/.glm.linear/.sumstats.tsv/')
     mv ${FILENAME} pqtl_associations/${NEWFILE}
 done
-
-# GLIPR1 only has 106 non-missing samples, and within that subset some covariates are constant or collinear.
-# Those covariates must be removed before performing a GWAS for this protein, or else PLINK will crash.
-plink2 --pfile data_cleaned/genotypes \
-       --no-psam-pheno \
-       --threads 128 \
-       --memory 420000 \
-       --pheno data_cleaned/proteins.tsv \
-       --pheno-name GLIPR1 \
-       --pheno-quantile-normalize \
-       --covar data_cleaned/covariates.tsv \
-       --not-covar ASSESSMENT_CENTER_11001 ASSESSMENT_CENTER_11002 ASSESSMENT_CENTER_11005 ASSESSMENT_CENTER_11018 ASSESSMENT_CENTER_11020 ASSESSMENT_CENTER_11022 ASSESSMENT_CENTER_11023 \
-       --covar-variance-standardize \
-       --glm omit-ref hide-covar cols=chrom,pos,beta,p \
-       --vif 100000 \
-       --out TEMP
-
-mv TEMP.GLIPR1.glm.linear pqtl_associations/GLIPR1.sumstats.tsv
 
 rm TEMP*
 
